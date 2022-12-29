8fdc55666922022b148f8346a5faa927f7eaf226
utgwkk <utagawakiki@gmail.com>
2022-12-29 14:26:56 +0900

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -10,5 +10,5 @@ export default function Home() {
       </Head>
       <h1>It works!</h1>
     </>
-  )
+  );
 }
