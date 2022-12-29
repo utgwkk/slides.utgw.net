8b05c2042860ba2ab81c13c600b1716468f80d8e
utgwkk <utagawakiki@gmail.com>
2022-12-29 14:35:10 +0900

    slides

---
+++ lib/slides.ts
@@ -0,0 +1,15 @@
+export type Slide = {
+  title: string;
+  url: string;
+};
+
+export const slides = new Map<string, Slide>([
+  // TODO: fill
+  [
+    "yapc-japan-online-2022",
+    {
+      title: "prototypeとjust epic. と私",
+      url: "/pdf/20220305-prototype-and-just-epic-and-me.pdf",
+    },
+  ],
+]);
